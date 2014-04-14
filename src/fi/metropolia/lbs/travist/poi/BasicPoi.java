300f4bfb6cbe41a34fe01d56ca951218e926472a
rojun <rojun.itu@gmail.com>
2014-04-14 08:26:43 +0300

    Decorator structure on POIs.

---
+++ src/fi/metropolia/lbs/travist/poi/BasicPoi.java
@@ -0,0 +1,27 @@
+package fi.metropolia.lbs.travist.poi;
+
+import org.mapsforge.core.model.LatLong;
+
+public class BasicPoi implements Poi {
+	private String title;
+	private String category;
+	private LatLong coordinates;
+	
+	public BasicPoi(String newTitle, String newCategory, LatLong newCoordinates) {
+		title = newTitle;
+		category = newCategory;
+		coordinates = newCoordinates;
+	}
+	
+	@Override
+	public void saveToTodoList() {
+		// visitor
+	}
+
+	@Override
+	public void drawOnMap() {
+		// TODO reference to the correspondent component to draw into
+		// either MapView or OverLay
+	}
+
+}
