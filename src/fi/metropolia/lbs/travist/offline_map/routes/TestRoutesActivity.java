3623e005126b0600af478d864d0eb01da99d7d35
rojun <rojun.itu@gmail.com>
2014-04-22 14:26:36 +0300

    moved the mapAdapter and route into other packages, and therefor had to change method permissions/privileges

---
+++ src/fi/metropolia/lbs/travist/offline_map/routes/TestRoutesActivity.java
@@ -0,0 +1,43 @@
+package fi.metropolia.lbs.travist.offline_map.routes;
+
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import fi.metropolia.lbs.travist.testing.QuickLog;
+
+import travist.pack.R;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+
+public class TestRoutesActivity extends Activity {
+	private final Fragment mRoutesFragment = new TestTravistMapViewAdapterFragment();
+
+	@SuppressLint("NewApi")
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.routes_activity);
+
+		if (savedInstanceState == null) {
+			FragmentTransaction fragmentTransaction = getFragmentManager()
+					.beginTransaction();
+			Log.d("Testing", "nav fragment");
+			fragmentTransaction.add(R.id.route_nav, new RouteNavFragment());
+			Log.d("Testing", "map fragment");
+			fragmentTransaction.add(R.id.test_route_mapview, mRoutesFragment);
+			fragmentTransaction.commit();
+
+			AndroidGraphicFactory.createInstance(getApplication());
+		}
+
+	}
+
+}
